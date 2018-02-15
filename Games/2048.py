98eaa0f2b664dee184a21d650d153ef9fc8b6f67
Even <evenmn@fys.uio.no>
2018-02-15 16:53:07 +0100

    Fixed 2048.py to make it run on python3

--- Games/2048.py
+++ Games/2048.py
@@ -1,10 +1,12 @@
 import numpy as np
 import random
 
-#---To fix---
-#Make program as universial as possible: adjust size with one click
-#Leaderboard can be performed more elegant with dictoneries
-#And leaderboard spacing can be fixed
+'''
+---To be fixed---
+- Make program as universial as possible: dynamic size
+- Leaderboard can be performed more elegant with dictoneries
+- And leaderboard spacing can be fixed
+'''
 
 #Variables
 N = 4                   #System size, NxN
@@ -62,11 +64,11 @@ class color:
 #---Printing matrix---
 def print_matrix(matrix):
     print (chr(27) + "[2J")     #Clear terminal 
-    print "\t"
-    print "\t",int(matrix[0,0]),"\t",int(matrix[0,1]),"\t",int(matrix[0,2]),"\t",int(matrix[0,3]),"\n"
-    print "\t",int(matrix[1,0]),"\t",int(matrix[1,1]),"\t",int(matrix[1,2]),"\t",int(matrix[1,3]),"\n"
-    print "\t",int(matrix[2,0]),"\t",int(matrix[2,1]),"\t",int(matrix[2,2]),"\t",int(matrix[2,3]),"\n"
-    print "\t",int(matrix[3,0]),"\t",int(matrix[3,1]),"\t",int(matrix[3,2]),"\t",int(matrix[3,3]),"\n"
+    print ("\t")
+    print ("\t",int(matrix[0,0]),"\t",int(matrix[0,1]),"\t",int(matrix[0,2]),"\t",int(matrix[0,3]),"\n")
+    print ("\t",int(matrix[1,0]),"\t",int(matrix[1,1]),"\t",int(matrix[1,2]),"\t",int(matrix[1,3]),"\n")
+    print ("\t",int(matrix[2,0]),"\t",int(matrix[2,1]),"\t",int(matrix[2,2]),"\t",int(matrix[2,3]),"\n")
+    print ("\t",int(matrix[3,0]),"\t",int(matrix[3,1]),"\t",int(matrix[3,2]),"\t",int(matrix[3,3]),"\n")
 print_matrix(matrix)            #Printing initial matrix
 
 #---Gives the leaderboard---
@@ -86,21 +88,21 @@ def leaderboard(name, point, filename):
     names_sorted = []; indices = list(reversed(indices))
     for i in range(len(names)):
         names_sorted.append(names[indices[i]])
-    print '--------------------------------------'
-    print 'Top 10 highscores of all time:'
-    print '--------------------------------------'
+    print ('--------------------------------------')
+    print ('Top 10 highscores of all time:')
+    print ('--------------------------------------')
     if len(names)>10:
         for i in range(10):
-            print '%d. %s\t \t\t\t %d'%(i+1,names_sorted[i],int(points[i]))
+            print ('%d. %s\t \t\t\t %d'%(i+1,names_sorted[i],int(points[i])))
     else:
         for i in range(len(names)):
-            print '%d. %s\t \t\t\t %d'%(i+1,names_sorted[i],int(points[i]))
-    print '--------------------------------------'
+            print ('%d. %s\t \t\t\t %d'%(i+1,names_sorted[i],int(points[i])))
+    print ('--------------------------------------')
 
 num_goal = 0            #Number of times goal is reached
 
 while True:
-    arrow = raw_input("Please use 'w', 's', 'a' or 'd' to move the tiles: ")
+    arrow = input("Please use 'w', 's', 'a' or 'd' to move the tiles: ")
     moves = 0           #Number of tiles moving by one swipe
     merges = 0          #Number of tiles merging by one swipe
 
@@ -378,13 +380,13 @@ while True:
                 randomindex = listfori.index(random.choice(listfori))
                 matrix[listfori[randomindex],listforj[randomindex]] = random.choice(pick_list)
                 print_matrix(matrix)
-                print "Points: ",int(points)
+                print ("Points: ",int(points))
             else:
                 matrix[listfori[0]][listforj[0]] = random.choice(pick_list)
                 print_matrix(matrix)
-                print "Points: ",int(points)
+                print ("Points: ",int(points))
         else:
-            print "Please swipe in another direction!"
+            print ("Please swipe in another direction!")
         
     else:
         d = 0
@@ -393,10 +395,10 @@ while True:
                 if matrix[i,j]==matrix[i+1,j] or matrix[i,j]==matrix[i,j+1]:
                     d += 1
         if d == 0:     
-            print color.RED + "Game over" + color.END
+            print (color.RED + "Game over" + color.END)
             name = raw_input("Please enter your name, mister: ")
             leaderboard(name, points, '2048_leaderboard.txt')
             break
 
     if num_goal > 0:
-        print "Congratulation, you've reached %.d for the %.d. time"%(goal,num_goal)
+        print ("Congratulation, you've reached %.d for the %.d. time"%(goal,num_goal))
